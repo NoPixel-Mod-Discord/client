a4806a3c188c6ae9dc790f2526349b49adab2617
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-07-14 03:56:06 -0700

    Removed Prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import getfilteredItems from "@lib/hashroot/get-streamers";
 import { getSession, signIn, signOut, useSession } from "next-auth/react";
 import { GetServerSideProps } from "next/types";
 
@@ -6,25 +5,11 @@ const Home = () => {
   const { data: session } = useSession();
 
   if (session) {
-    const checkVerification = async () => {
-      try {
-        await fetch(`/api/add-record`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" }
-        });
-      } catch (e) {
-        alert(e);
-      }
-    };
     return (
       <>
         <p>
           Signed in as {session?.user.id} / {session?.user.name}
         </p>
-
-        <div>
-          <button onClick={() => checkVerification()}>Connect</button>
-        </div>
 
         <button onClick={() => signOut()}>Sign out</button>
       </>
@@ -42,27 +27,6 @@ export default Home;
 
 export const getServerSideProps: GetServerSideProps = async context => {
   const session = await getSession(context);
-  await getfilteredItems();
-
-  // const response = await prisma.streamers.findMany();
-  // const streamerList = response.map(streamer => streamer.channelId);
-
-  // const twitchClient: Client = new tmi.client({
-  //   connection: {
-  //     reconnect: true,
-  //     secure: true
-  //   },
-  //   channels: streamerList
-  // });
-
-  // twitchClient.on("connected", () => {
-  //   console.log("Connected");
-  // });
-  // twitchClient.on("message", (channel, tags, message, self) => {
-  //   console.log(channel, message);
-  // });
-
-  // twitchClient.connect();
 
   return {
     props: {
